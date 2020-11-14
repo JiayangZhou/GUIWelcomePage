49ec483a768466d27e25c2c6afc391fc6f9394e0
Jiayang <dengdaiyigeren@icloud.com>
2020-11-14 12:01:05 +0100

    added icons and welcome text

--- src/com/spaceship/WelcomeFrame.java
+++ src/com/spaceship/WelcomeFrame.java
@@ -6,13 +6,25 @@ import java.awt.*;
 public class WelcomeFrame extends JFrame {
     public WelcomeFrame(){
         this.setVisible(true);
-        this.setSize(430,430);
+        this.setSize(1200,900);
+        this.setLocation(250,250);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        ImageIcon imageIcon = new ImageIcon("/Users/jiayangzhou/Tea/Photos/Logo/xiaoxin.jpg");
+        ImageIcon imageIcon = new ImageIcon("src/com/spaceship/image/xiaoxin.jpg");
+
         this.setIconImage(imageIcon.getImage());
         this.getContentPane().setBackground(new Color(255,103,32));
 
+        //ImageIcon bigSurIcon = new ImageIcon("src/com/spaceship/image/bigSur.png");
+
         JLabel label = new JLabel();
+        label.setIcon(imageIcon);
+        label.setText("Welcome to Mac Big Sur");
+        //label.setHorizontalAlignment(JLabel.CENTER);
+        //label.setHorizontalAlignment(JLabel.TOP);
+
+        this.add(label);
+
+
     }
 }
